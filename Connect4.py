0df43789f14da6cd3ec6ba06ed575e3c607fc0fa
Donghoon Han <dhan@albany.edu>
2018-10-04 13:07:13 -0400

    Fix winning condition

--- Connect4.py
+++ Connect4.py
@@ -34,24 +34,24 @@ def print_board(board):
 #    pass
     
 def winning(board, piece):
-    for i in range(Num_Col-3): #last 3 is not gonna work (vertical)
+    for i in range(Num_Col-3): #last 3 is not gonna work (horizontal)
         for j in range(Num_Row):
-            if board[j][i] == piece and board[j][i+1] == piece and board[j][i+2] and board[j][i+3] == piece:
+            if board[j][i] == piece and board[j][i+1] == piece and board[j][i+2] == piece and board[j][i+3] == piece:
                 return True
     
-    for i in range(Num_Col): #last 3 is not gonna work (horizontal)
+    for i in range(Num_Col): #last 3 is not gonna work (vertical)
         for j in range(Num_Row-3):
-            if board[j][i] == piece and board[j+1][i] == piece and board[j+2][i] and board[j+3][i] == piece:
+            if board[j][i] == piece and board[j+1][i] == piece and board[j+2][i] == piece and board[j+3][i] == piece:
                 return True
             
     for i in range(Num_Col-3): #last 3 is not gonna work (positive diagonal)
         for j in range(Num_Row-3):
-            if board[j][i] == piece and board[j+1][i+1] == piece and board[j+2][i+2] and board[j+3][i+3] == piece:
+            if board[j][i] == piece and board[j+1][i+1] == piece and board[j+2][i+2] == piece and board[j+3][i+3] == piece:
                 return True
             
     for i in range(Num_Col-3): #last 3 is not gonna work (negative diagonal)
         for j in range(3, Num_Row):
-            if board[j][i] == piece and board[j-1][i+1] == piece and board[j-2][i+2] and board[j-3][i+3] == piece:
+            if board[j][i] == piece and board[j-1][i+1] == piece and board[j-2][i+2] == piece and board[j-3][i+3] == piece:
                 return True
 
 board = create_board()
@@ -86,7 +86,7 @@ while not game_over:
             row = next_row(board, col)
             drop(board, row, col, 2)
             
-            if winning(board,1):
+            if winning(board,2):
                 print("Player2 wins!")
                 game_over = True
     
